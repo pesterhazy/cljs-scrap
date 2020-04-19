8ab7654b063bcf9d11c49a72b03170d597c77d15
Paulus Esterhazy <pesterhazy@gmail.com>
2020-04-19 12:28:52 +0200

    WIP

--- src/main/scrap/scrap.cljs
+++ src/main/scrap/scrap.cljs
@@ -1,4 +1,30 @@
 (ns scrap.scrap)
 
+(defn make-world []
+  {:!critical (atom nil)})
+
+
+#_(defn write+ [world path v]
+    (-> (js/Promise.reolve)
+        ;; TODO: delay?
+        (.then (fn []
+                 (swap! (:!state o) assoc-in path v)))))
+
+(defn banana+ [{:keys [!critical]}]
+  (-> (js/Promise.resolve)
+      (.then (fn []
+               (when @!critical
+                 (throw (js/Error. "Another process is already in critical section")))
+               (prn [::enter])
+               (reset! !critical true)))
+      (.then (fn []
+               ;; wait?
+               ))
+      (.then (fn []
+               (prn [::leave])
+               (reset! !critical false)))))
+
 (defn ^:export main []
-  (prn :main))
+  (-> (banana+ (make-world))
+      (.then (fn []
+               (prn :ok)))))
