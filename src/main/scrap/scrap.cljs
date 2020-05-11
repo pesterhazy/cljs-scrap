837b679624c80a290b99aeeb104921877b63e125
Paulus Esterhazy <pesterhazy@gmail.com>
2020-05-11 09:07:38 +0200

    WIP

--- src/main/scrap/scrap.cljs
+++ src/main/scrap/scrap.cljs
@@ -5,10 +5,25 @@
             [reagent.core :as r]
             [scrap.dijkstra]))
 
-(defn <root> []
+(defonce !tick (r/atom 0))
+
+(defn <clock> []
   [:div
-   [:h1 "Hello"]
-   [:div "world"]])
+   "clock:"
+   @!tick])
+
+(defn <root>
+  []
+  (r/with-let [!timer (atom nil)]
+    [:div {:ref (fn [e]
+                  (if e
+                    (reset! !timer (js/setInterval (fn []
+                                                     (swap! !tick inc)
+                                                     (js/console.log @!tick))
+                                                   1000))
+                    (js/clearInterval @!timer)))}
+     [:h1 "Hello"]
+     [<clock>]]))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (defn ^:export main []
