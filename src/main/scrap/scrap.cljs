5157185a34d3448f86fe840a081bc7db6f3c12c6
Paulus Esterhazy <pesterhazy@gmail.com>
2020-05-16 13:21:39 +0200

    WIP

--- src/main/scrap/scrap.cljs
+++ src/main/scrap/scrap.cljs
@@ -2,39 +2,17 @@
   (:require-macros [scrap.ppp])
   (:require [clojure.test :as t]
             [clojure.pprint]
-            [reagent.core :as r]
+            [reagent.dom :as rdom]
+            ["react" :as React]
             [scrap.dijkstra]))
 
-
-(defonce !tick (r/atom 0))
-
-(defn <clock-1> []
-  (r/with-let [tick @!tick]
-    [:div
-     "clock:"
-     tick]))
-
-(defn <clock-2> []
-  [:div
-   "clock:"
-   @!tick])
+(js/console.log (.-version React))
 
 (defn <root>
   []
-  (r/with-let [!timer (atom nil)]
-    [:div {:ref (fn [e]
-                  (if e
-                    (reset! !timer (js/setInterval (fn []
-                                                     (swap! !tick inc)
-                                                     (js/console.log @!tick))
-                                                   1000))
-                    (js/clearInterval @!timer)))}
-     [:h1 "Hello"]
-     [<clock-1>]
-     [<clock-2>]]))
+  [:div
+   [:h1 "Hello"]])
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (defn ^:export main []
-  (r/render [<root>] (js/document.getElementById "app"))
-
-  #_(t/run-tests 'scrap.dijkstra))
+  (rdom/render [<root>] (js/document.getElementById "app")))
