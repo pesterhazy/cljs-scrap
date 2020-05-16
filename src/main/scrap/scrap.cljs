4ba12988f7969e23f7da4bf53a6d8e3716f83e9c
Paulus Esterhazy <pesterhazy@gmail.com>
2020-05-16 19:19:02 +0200

    WIP

--- src/main/scrap/scrap.cljs
+++ src/main/scrap/scrap.cljs
@@ -5,25 +5,33 @@
             [clojure.string :as str]
             ["react" :as react :refer [useRef useEffect]]
             ["react-dom" :as react-dom]
+            ["recoil" :as recoil]
             [reagent.core :as r]
             [reagent.dom :as rdom]
             ["@use-it/interval" :as use-interval]
             [scrap.dijkstra]))
 
-(defn clock []
-  (js/console.log "clock")
-  (let [[cnt set-cnt] (react/useState (js/Date.now))]
-    ;; If you replace use-interval with my-use-interval, things break
-    ;; - why?  There must be some subtle difference between
-    ;; use-interval and my-use-interval but I can't see it.
-    (use-interval (fn []
-                    (let [new-cnt (js/Date.now)]
-                      (js/console.log "new-cnt" new-cnt)
-                      (set-cnt new-cnt)))
-                  1000)
-    [:div cnt]))
+(defonce !text (recoil/atom #js{:key "text"
+                                :default "x"}))
+
+(defn <text> []
+  (let [text (recoil/useRecoilValue !text)]
+    [:div "Text: " (pr-str text)]))
+
+(defn <main> []
+  (let [set-text (recoil/useSetRecoilState !text)]
+    [:div
+     [:h1 "welcome"]
+     [:f> <text>]
+     [:button {:style {:margin-top 10}
+               :on-click (fn [] (set-text (fn [s] (str s "x"))))}
+      "more"]]))
+
+(defn <root> []
+  [:> recoil/RecoilRoot
+   [:f> <main>]])
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (defn ^:export main []
-  (rdom/render [:f> clock] (js/document.getElementById "app")))
+  (rdom/render [:f> <root>] (js/document.getElementById "app")))
