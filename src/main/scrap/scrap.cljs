c856bdc51cfef72ac892979afd7412a63f992499
Paulus Esterhazy <pesterhazy@gmail.com>
2021-02-28 14:23:19 +0100

    WIP

--- src/main/scrap/scrap.cljs
+++ src/main/scrap/scrap.cljs
@@ -2,36 +2,11 @@
   (:require-macros [scrap.ppp])
   (:require [clojure.test :as t]
             [clojure.pprint]
-            [reagent.core :as r]
-            [scrap.dijkstra]))
-
-
-(defonce !tick (r/atom 0))
-
-(defn <clock-1> []
-  (r/with-let [tick @!tick]
-    [:div
-     "clock:"
-     tick]))
-
-(defn <clock-2> []
-  [:div
-   "clock:"
-   @!tick])
+            [reagent.core :as r]))
 
 (defn <root>
   []
-  (r/with-let [!timer (atom nil)]
-    [:div {:ref (fn [e]
-                  (if e
-                    (reset! !timer (js/setInterval (fn []
-                                                     (swap! !tick inc)
-                                                     (js/console.log @!tick))
-                                                   1000))
-                    (js/clearInterval @!timer)))}
-     [:h1 "Hello"]
-     [<clock-1>]
-     [<clock-2>]]))
+  [:div "hello"])
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (defn ^:export main []
